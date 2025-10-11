b23cd17b92dd23c49588b1f678c3c602dfcf73f0
thanhvo <vochithanh2804@gmail.com>
2025-10-11 12:20:13 +0700

    add reaction script for drinkmaker game

--- Assets/Scripts/DrinkMaker/TrackBar.cs
+++ Assets/Scripts/DrinkMaker/TrackBar.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TrackBar : MonoBehaviour
 {
     [SerializeField] GameObject runner;
     [SerializeField] GameObject sweetSpot;
+    private InputAction getSpacePress;
+
+    void OnEnable()
+    {
+        // Create an action that listens for the Space key
+        getSpacePress = new InputAction(type: InputActionType.Button, binding: "<Keyboard>/space");
+        getSpacePress.performed += spacePressCallback;
+        getSpacePress.Enable();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,6 +28,32 @@ public class TrackBar : MonoBehaviour
         if (runner.transform.position.x > 7.0f)
         {
             runner.transform.position = new Vector3(-6.0f, 0, 0);
+        }
+    }
+
+    void spacePressCallback(InputAction.CallbackContext ctx)
+    {
+        //  float y = bound_sweetSpot.size.y;
+        // if Space pressed
+        if (ctx.performed)
+        {
+            SpriteRenderer SR_sweetSpot = sweetSpot.GetComponent<SpriteRenderer>();
+            Bounds bound_sweetSpot = SR_sweetSpot.bounds;
+            float edgeToMiddleDis = bound_sweetSpot.size.x / 2;
+            Vector2 runnerPosition = runner.transform.position;
+            Vector2 sweetSpotPosition = sweetSpot.transform.position;
+            float yesAreaPos = sweetSpotPosition.x + edgeToMiddleDis;
+            float yesAreaNeg = sweetSpotPosition.x - edgeToMiddleDis;
+
+            if (runnerPosition.x >= yesAreaNeg &&
+                runnerPosition.x <= yesAreaPos)
+            {
+                Debug.Log("SCORED!!!");
+            }
+            else
+            {
+                Debug.Log("Noooob!!");
+            }
         }
     }
 }
